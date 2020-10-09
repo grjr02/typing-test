e08629e4cb0361fca2f1bbde07945b3b1980b814
grjr02 <grjr02@outlook.com>
2020-10-08 20:58:29 -0400

    timer component

---
+++ src/components/Timer.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Timer() {
+    const [count, setCount] = useState(59);
+    const [time, setTime] = useState("01:00");
+
+    const [color, setColor] = useState('#DBDBDB');
+
+
+    useEffect(() => {
+        
+        const timeout = setTimeout(timer, 1000)
+
+        if(count === 0){
+            setTime(`00:0${count}`)
+            clearTimeout(timeout);
+            setColor("rgba(255, 0, 0, .25)")
+        }
+
+    },[count])
+
+    const timer = () => {
+        setCount(count-1);
+
+        if(count >= 10) setTime(`00:${count}`)
+        else setTime(`00:0${count}`)
+        
+    }
+    return (
+        <div className="timercard" style={{border:`2px solid ${color}` }}>
+            <span className="timerText">
+                {time}
+            </span> 
+                
+           
+        </div>
+    )
+}
